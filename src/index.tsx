7c77e195e5738c5f92a785a12982e50a99798a84
SangDon <adoim@naver.com>
2022-07-12 23:21:17 +0900

    status1

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App";
+
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
