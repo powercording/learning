fddd92fd5e9d807fbb4608bd7729e87e58280ca5
SangDon <adoim@naver.com>
2022-07-13 23:46:19 +0900

    doingCoins

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,9 +1,37 @@
 import styled from "styled-components";
 
-const Title = styled.h1`
-  color: ${(props) => props.theme.focusedColor};
+const Container = styled.div`
+  display: grid;
+  /* grid-template: ; */
 `;
+const NameWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+const CoinWrapper = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+const Header = styled.h1`
+  color: ${(props) => props.theme.textColor};
+  font-size: 2rem;
+  padding: 5px;
+`;
+const CoinName = styled.h2`
+  color: ${(props) => props.theme.focusedColor};
+  padding-left: 5px;
+`;
+
 function Coins() {
-  return <Title>coins</Title>;
+  return (
+    <Container>
+      <NameWrapper>
+        <Header>coins</Header>
+        <CoinName>names</CoinName>
+      </NameWrapper>
+      <CoinWrapper></CoinWrapper>
+    </Container>
+  );
 }
 export default Coins;
