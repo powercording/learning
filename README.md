a8edca8b41c66d5f94dd43765c510404a5546ae4
SangDon <adoim@naver.com>
2022-10-25 00:33:47 +0900

    pull

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-#안녕하세요 TIL 입니다.
+# 안녕하세요 TIL 입니다.
 
+## 클론 공부 배포링크입니다. 서버가 없기 때문에 간단하게 동작만 시험해 볼 수 있습니다.
 
+[todoList with drag and drop animaion][todo]
+[todo]:[https://webdevfront.com/react_to_do/]
+
+[movieClon:netfilx][moive]
+[moive]:[https://webdevfront.com/movieclone_build/]
--- clone/movieClone/src/Routes/Tv.tsx
+++ clone/movieClone/src/Routes/Tv.tsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
+
 function Tv() {
   const REST_API_KEY = "7c696e3a3c14642e58cb3002d74b5488";
   const REDIRECT_URI = "http://localhost:3000/movieclone_build/token";
+  const [Cnumber, setCnumber] = useState<number[]>([0, 0, 0]);
+
+  const computerNumber = () => {
+    Cnumber.map((item, index) => {
+      const newNum = Math.ceil(Math.random() * 10);
+      if (!Cnumber.includes(newNum)) {
+      }
+    });
+  };
+
+  const generateRandom: number[] = () => {
+    
+    return [0, 0];
+  };
 
   return (
     <div style={{ marginTop: "150px" }}>
-      <a
-        href={`https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`}
-      >
-        dd
-      </a>
+      <div style={{ backgroundColor: "white" }}>
+        <h1>Computer Number : </h1>
+        <span>{Cnumber}</span>
+        <button onClick={computerNumber}>컴퓨터 번호 생성</button>
+      </div>
     </div>
   );
 }
